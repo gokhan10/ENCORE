c0e2dde9a9c151e69851def5610d0193e4a32ddd
gokhan <gokhan@agyoneticileri.org>
2021-03-21 19:04:11 +0300

    Pycharm uygulamasi

---
+++ Env_Lab.py
@@ -0,0 +1,10 @@
+# Set Env
+ENVONMENT_IN_USE = "sandbox"
+if ENVONMENT_IN_USE == "sandbox":
+     dnac={
+        "host": "sandboxdnac.cisco.com",
+        "port": 443,
+        "username": "devnetuser",
+        "password": "Cisco123!"
+  }
+
